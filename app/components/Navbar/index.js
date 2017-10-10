725326b8470f72cc4cebae214848d716a4dd4745
brandonglennt <brandonglennt@gmail.com>
2017-10-10 16:13:25 -0400

    Created Services Page

--- app/components/Navbar/index.js
+++ app/components/Navbar/index.js
@@ -68,7 +68,7 @@ export default class Navbar extends React.PureComponent {
       <div>
         <nav className="navBar">
           <div className="siteName">
-            <img src={require("../../photos/logo.png")} className="homeLogo"/>
+            <img src={require("../../photos/logo-white.png")} className="homeLogo"/>
           </div>
           <div className="nav">
             <Link to="/" className="navButton">HOME</Link>
