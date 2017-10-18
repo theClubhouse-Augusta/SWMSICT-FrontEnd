546e68150312763ad70bd8a58e8c2f37049af077
Ed <Ed@Eds-MacBook-Air.local>
2017-10-18 16:47:19 -0400

    added Mike's stuff

--- app/components/UserInfoContribution/index.js
+++ app/components/UserInfoContribution/index.js
@@ -21,21 +21,28 @@ export default class UserInfoContribution extends React.PureComponent {
     sessionStorage.setItem('isIndexFund', 0);
     sessionStorage.setItem('isRetirement', 0);
   }
-  render() {
-    return (
-      <div className="contributionContainer">
-        <div className="userInfoContribution">
-          <h1>
-            <div>How much are you</div>
-            <div>interested in investing?</div>
-          </h1>
-          <span><span className="dollar">$</span><input id="dollar" type="number" min="00.00" max="9999999" step="50" placeholder=""/></span>
-          <input type="button" value="Continue" onClick={this.props.handleContinue1}/>
+  handleEnter = (event) => {
+      if(event.keyCode === 13)
+      {
+        this.props.handleContinue1();
+      }
+    };
+
+    render() {
+      return (
+        <div className="contributionContainer">
+          <div className="userInfoContribution">
+            <h1>
+              <div>How much are you</div>
+              <div>interested in investing?</div>
+            </h1>
+            <span><span className="dollar">$</span><input id="dollar" type="number" min="00.00" max="9999999" step="50" placeholder="" onKeyDown={this.handleEnter}/></span>
+            <input type="button" value="Continue" onClick={this.props.handleContinue1}/>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
 
 UserInfoContribution.contextTypes = {
   router: React.PropTypes.object
--- app/components/UserInfoRisk/index.js
+++ app/components/UserInfoRisk/index.js
@@ -10,7 +10,54 @@ import './style.css';
 import './styleM.css';
 
 export default class UserInfoRisk extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      activeBox:0
+    }
+  }
+
+  handleRiskClick = (event) => {
+    if(event.target.value == 3)
+    {
+      this.setState({
+        activeBox:3
+      })
+    }
+    else if(event.target.value == 2)
+    {
+      this.setState({
+        activeBox:2
+      })
+    }
+    else if(event.target.value == 1)
+    {
+      this.setState({
+        activeBox:1
+      })
+    }
+    this.props.handleRiskLevel(event);
+  }
+
   render() {
+
+    let riskRadio1 = "riskRadio";
+    let riskRadio2 = "riskRadio";
+    let riskRadio3 = "riskRadio";
+
+    if(this.state.activeBox === 1)
+    {
+      riskRadio1 = "riskRadioActive";
+    }
+    else if(this.state.activeBox === 2)
+    {
+      riskRadio2 = "riskRadioActive";
+    }
+    else if(this.state.activeBox === 3)
+    {
+      riskRadio3 = "riskRadioActive";
+    }
+
     return (
       <div className="riskContainer">
         <div className="userInfoRisk">
@@ -21,12 +68,12 @@ export default class UserInfoRisk extends React.PureComponent {
 
 
           <div className="riskChoice">
-              <label className="riskRadio">
-                <input type="radio" name="risk" value="3" onClick={this.props.handleRiskLevel} hidden/>Conservative</label>
-              <label className="riskRadio">
-                <input type="radio" name="risk" value="2" onClick={this.props.handleRiskLevel} hidden/>Moderate</label>
-              <label className="riskRadio">
-                <input type="radio" name="risk" value="1" onClick={this.props.handleRiskLevel} hidden/>Aggressive</label>
+              <label className={riskRadio3}>
+                <input  type="radio" name="risk" value="3" onClick={this.handleRiskClick} hidden/>Conservative</label>
+              <label className={riskRadio2}>
+                <input type="radio" name="risk" value="2" onClick={this.handleRiskClick} hidden/>Moderate</label>
+              <label className={riskRadio1}>
+                <input type="radio" name="risk" value="1" onClick={this.handleRiskClick} hidden/>Aggressive</label>
           </div>
           <input type="button" value="Continue" onClick={this.props.handleContinue2}/>
         </div>
